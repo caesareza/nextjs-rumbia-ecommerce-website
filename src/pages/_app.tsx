e07d758d873978fa73319fb3d370ba02a142a75e
dreas <andreas.caesareza@erajaya.com>
2024-08-28 10:02:38 +0700

    add linter and prettier

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import MainLayout from '@/components/layouts/MainLayout'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
+    console.log('asd')
     return (
         <MainLayout>
             <Component {...pageProps} />
