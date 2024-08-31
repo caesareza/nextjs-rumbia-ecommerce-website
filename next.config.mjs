16588ae5c448032dfa154fb2b53fe75857a18944
dreas <andreas.caesareza@erajaya.com>
2024-08-31 09:24:43 +0700

    Setting up Environment variable

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
+const REACT_STRICT_MODE_ACTIVE_ON_DEVELOPMENT = process.env.NEXT_PUBLIC_APP_ENV === 'development'
+
 const nextConfig = {
-    reactStrictMode: true,
+    reactStrictMode: REACT_STRICT_MODE_ACTIVE_ON_DEVELOPMENT,
+    experimental: {
+        webVitalsAttribution: ['CLS', 'LCP']
+    }
 }
 
 export default nextConfig
